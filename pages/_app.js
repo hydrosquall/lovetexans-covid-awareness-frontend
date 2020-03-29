e641586919f5d2cdaf22aaa74176598708c0f62b
Cameron Yick <cameron.yick@gmail.com>
2020-03-29 15:30:34 -0400

    shrink bundle by removing css

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,7 @@
 // CSS
 import App from "next/app";
 
-// Alex custom styles
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/css/bootstrap-theme.min.css";
 import "semantic-ui-css/semantic.min.css";
-
 import ReactGA from "react-ga";
 import { datadogRum } from "@datadog/browser-rum";
 
@@ -13,7 +9,6 @@ import { datadogRum } from "@datadog/browser-rum";
 // https://stackoverflow.com/questions/55151041/window-is-not-defined-in-nextjs-react-app/55151122
 const ANALYTICS_ID = "UA-134441849-4";
 if (process.browser) {
-
   ReactGA.initialize(ANALYTICS_ID);
   ReactGA.pageview(window.location.pathname + window.location.search);
 
